15d4f99be30752d9366c72aee07fec3ff50de2b2
VIT <david.padrino@wikot.com>
2017-03-25 17:56:02 -0400

    pruebas de conexion

--- client/main.js
+++ client/main.js
@@ -37,10 +37,8 @@ Template.hello.events({
     /*Ḥow to return a value from server*/
     Meteor.call('test2',function(error, result){
        	if(error){
-            console.log("hay un error ");
             console.log(error);
         } else {
-            console.log("hay result ");
             console.log(result);
         }
     });
